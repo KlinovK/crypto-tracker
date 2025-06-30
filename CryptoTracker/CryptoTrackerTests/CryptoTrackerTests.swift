43e8e37f225230f34f8b2cf0860afd292da69d62
kost9klinov@gmail.com <kost9klinov@gmail.com>
2025-06-30 09:52:17 +0800

    [+] Init commit

---
+++ CryptoTracker/CryptoTrackerTests/CryptoTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerTests.swift
+//  CryptoTrackerTests
+//
+//  Created by Константин Клинов on 30/06/25.
+//
+
+import Testing
+@testable import CryptoTracker
+
+struct CryptoTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
